28ca1a93f04ebd1374e726573c6b897eda40c395
Snezha95 <snezha.95@mail.ru>
2018-05-28 01:20:46 +0700

    add two classes set pause and get pause

--- GameInCity/GameInCity/Game.cs
+++ GameInCity/GameInCity/Game.cs
@@ -11,6 +11,7 @@ namespace GameInCity
     {
         private string[] cities = null;
         private string name_gamer1 = null;
+        private int pause = 10;
         public bool Load()
         {
             try
@@ -21,5 +22,7 @@ namespace GameInCity
             catch { return false; }
         }
         public void SetNameGamer1(string name) { name_gamer1 = name; }
+        public void SetPause(int pause) { this.pause = pause; }
+        public int GetPause() { return pause; }
     }
 }
