860996b6eb7ded45537b0ce4c86589c227ba425b
Snezha95 <snezha.95@mail.ru>
2018-05-28 00:10:25 +0700

    create test load list

---
+++ GameInCity/Test/TDD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class TDD
+    {
+        [TestMethod]
+        public void TestMethod_Load()
+        {
+            Game game = new Game();
+            bool rezult = game.Load();
+            Assert.AreEqual(true, rezult);
+        }
+    }
+}
