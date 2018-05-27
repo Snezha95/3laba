8b8cf2b94909e5a485e2195a8aee7aeb96a180d9
Snezha95 <snezha.95@mail.ru>
2018-05-28 02:14:19 +0700

    verification system pause and cities out of list

--- GameInCity/GameInCity/Program.cs
+++ GameInCity/GameInCity/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace GameInCity
 {
@@ -17,6 +18,32 @@ namespace GameInCity
                 game.SetNameGamer1(Console.ReadLine());
                 Console.WriteLine("Имя второго игрока: ");
                 game.SetNameGamer2(Console.ReadLine());
+                game.SetPause(5);
+                int pause = game.GetPause();
+                for (; pause > 0; pause--)
+                {
+                    Console.Write("Ходит игрок {0}. Осталось времени подумать: {1}\r", game.GetNameCurrentGamer(), pause);
+                    Thread.Sleep(1000);
+                }
+                Console.Write("                                                                                      \r");
+                Console.Write("Назовите город: ");
+                string city = Console.ReadLine();
+                bool end = false;
+                if (!game.SearchCity(city))
+                {
+                    Console.WriteLine("Такого города нет!");
+                    end = true;
+                }
+                else
+                {
+                    game.NextGamer();
+                }
+                if (end)
+                {
+                    Console.WriteLine("Игрок {0} проиграл!", game.GetNameCurrentGamer());
+                    Console.ReadKey();
+                    return;
+                }
             }
             else
                 Console.WriteLine("Не найден файл со списком городов!");
