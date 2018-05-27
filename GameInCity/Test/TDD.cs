8cef8a050195e5e75e4157532e6390c395839c6f
Snezha95 <snezha.95@mail.ru>
2018-05-28 01:45:38 +0700

    create test name current gamer

--- GameInCity/Test/TDD.cs
+++ GameInCity/Test/TDD.cs
@@ -53,5 +53,13 @@ namespace Test
             int rezult = game.GetCurrentGamer();
             Assert.AreEqual(2, rezult);
         }
+        [TestMethod]
+        public void TestMethod_NameCurrentGamer()
+        {
+            Game game = new Game();
+            game.SetNameGamer1("123");
+            string rezult = game.GetNameCurrentGamer();
+            Assert.AreEqual("123", rezult);
+        }
     }
 }
