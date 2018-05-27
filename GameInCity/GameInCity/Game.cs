c24680ce3bdb9c3d1b9b34112e85e0f12d8c599e
Snezha95 <snezha.95@mail.ru>
2018-05-28 00:29:21 +0700

    add class gamer1

---
+++ GameInCity/GameInCity/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GameInCity
+{
+   public class Game
+    {
+        private string[] cities = null;
+        private string name_gamer1 = null;
+        public bool Load()
+        {
+            try
+            {
+                cities = File.ReadAllLines("../../../cities.txt", Encoding.GetEncoding(1251));
+                return true;
+            }
+            catch { return false; }
+        }
+        public void SetNameGamer1(string name) { name_gamer1 = name; }
+    }
+}
